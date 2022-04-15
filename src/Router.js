b5f9350032820de64335e8e8498b32bee5575bd8
Yaifer <JHANNON@madisoncollege.edu>
2022-04-15 11:44:24 -0500

    Created Router component

--- src/Router.js
+++ src/Router.js
@@ -1,23 +1,25 @@
-import React, { useState, useEffect } from 'react'
-import { HashRouter, Switch, Route } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 
-import Nav from './Nav'
-import Public from './Public'
-import Profile from './Profile'
-import Protected from './Protected'
+import Nav from './Nav';
+import Public from './Public';
+import Profile from './Profile';
+import Protected from './Protected';
 
 const Router = () => {
-  const [current, setCurrent] = useState('home')
-  useEffect(() => {
-    setRoute()
-    window.addEventListener('hashchange', setRoute)
-    return () =>  window.removeEventListener('hashchange', setRoute)
-  }, [])
-  function setRoute() {
-    const location = window.location.href.split('/')
-    const pathname = location[location.length-1]
-    setCurrent(pathname ? pathname : 'home')
-  }
+  const [current, setCurrent] = useState('home');
+  useEffect(
+    () => {
+    setRoute();
+    window.addEventListener('hashchange', setRoute);
+    return () =>  window.removeEventListener('hashchange', setRoute);
+  }, []);
+  const setRoute = () => {
+      const location = window.location.href.split('/');
+      const pathname = location[location.length-1];
+      
+      setCurrent(pathname ? pathname : 'home');
+  };
   return (
     <HashRouter>
       <Nav current={current} />
@@ -28,7 +30,7 @@ const Router = () => {
         <Route component={Public}/>
       </Switch>
     </HashRouter>
-  )
-}
+  );
+};
 
-export default Router
+export default Router;
